9c318b786b2790edd6ac4268b31582d329a601db
Ali najafian <alinajafian580@gmail.com>
2024-05-10 17:14:15 +0330

    Completed AuthenticationRequest.java and the subclass (RegisterationRequest)

--- src/main/java/ir/najaftech/najafer/UserAuthentication/AuthenticationRequest.java
+++ src/main/java/ir/najaftech/najafer/UserAuthentication/AuthenticationRequest.java
@@ -6,6 +6,26 @@ public class AuthenticationRequest {
     private String userEmail;
     private String userPassword;
 
+    public AuthenticationRequest(String userEmail, String userPassword) {
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
 
     public class RegisterationRequest {
         private String userEmail;
@@ -13,5 +33,49 @@ public class AuthenticationRequest {
         private LocalDate dob;
         private String name;
         private String lastName;
+
+        
+
+        public RegisterationRequest(String userEmail, String userPassword, LocalDate dob, String name,
+                String lastName) {
+            this.userEmail = userEmail;
+            this.userPassword = userPassword;
+            this.dob = dob;
+            this.name = name;
+            this.lastName = lastName;
+        }
+
+        public String getUserEmail() {
+            return userEmail;
+        }
+        public void setUserEmail(String userEmail) {
+            this.userEmail = userEmail;
+        }
+        public String getUserPassword() {
+            return userPassword;
+        }
+        public void setUserPassword(String userPassword) {
+            this.userPassword = userPassword;
+        }
+        public LocalDate getDob() {
+            return dob;
+        }
+        public void setDob(LocalDate dob) {
+            this.dob = dob;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getLastName() {
+            return lastName;
+        }
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        
     }
 }
