94bdf2fc42a89281e2cfa4e3a641a83e7fdc3d97
Ali najafian <alinajafian580@gmail.com>
2024-05-11 19:15:57 +0330

    Implemented register method in UserAuthenticationController

--- src/main/java/ir/najaftech/najafer/UserAuthentication/UserAuthenticationController.java
+++ src/main/java/ir/najaftech/najafer/UserAuthentication/UserAuthenticationController.java
@@ -1,5 +1,6 @@
 package ir.najaftech.najafer.UserAuthentication;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("api/v1/user/login")
+@RequestMapping("api/v1/user/auth/")
 public class UserAuthenticationController {
 
     private UserAuthenticationService userAuthenticationService;
@@ -16,11 +17,16 @@ public class UserAuthenticationController {
         this.userAuthenticationService = userAuthenticationService;
     }
 
-    @PostMapping
+    @PostMapping("login")
     public ResponseEntity<UserAuthenticationResponse> login(@RequestBody UserAuthenticationRequest credentials) {
         if (credentials.getUserEmail().isEmpty() || credentials.getUserPassword().isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
         return ResponseEntity.ok(userAuthenticationService.authenticate(credentials));
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<UserAuthenticationResponse> register(@RequestBody UserAuthenticationRequest.RegisterationRequest credentials) {
+        return ResponseEntity.ok(userAuthenticationService.register(credentials)); //TODO : fix the response entity to return the actual URI path and not just a plain response entity of 200
     }
 }
