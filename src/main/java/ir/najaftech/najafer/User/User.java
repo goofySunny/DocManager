3bc2038398332e2fa8f4e8957792617ab69964b1
Ali najafian <alinajafian580@gmail.com>
2024-05-11 17:28:01 +0330

    Added username field (member variable) to UserAuthenticationResponse

--- src/main/java/ir/najaftech/najafer/User/User.java
+++ src/main/java/ir/najaftech/najafer/User/User.java
@@ -92,7 +92,6 @@ public class User implements UserDetails{
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
     }
 
--- src/main/java/ir/najaftech/najafer/UserAuthentication/UserAuthenticationResponse.java
+++ src/main/java/ir/najaftech/najafer/UserAuthentication/UserAuthenticationResponse.java
@@ -3,9 +3,19 @@ package ir.najaftech.najafer.UserAuthentication;
 
 public class UserAuthenticationResponse {
     private String token;
+    private String username;
 
-    public UserAuthenticationResponse(String token) {
+    public UserAuthenticationResponse(String token, String username) {
         this.token = token;
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getToken() {
