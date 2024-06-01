669b97df95190889df1f61912f5263b213761b08
Ali najafian <alinajafian580@gmail.com>
2024-06-01 21:03:15 +0330

    this bug fix was the worst

--- src/main/java/ir/najaftech/najafer/Doctor/DoctorController.java
+++ src/main/java/ir/najaftech/najafer/Doctor/DoctorController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/doctors")
