7dea03241069318c42689b535337f902fdbd6966
Ali najafian <alinajafian580@gmail.com>
2024-06-04 15:34:18 +0330

    ReservationController dependency injected

--- src/main/java/ir/najaftech/najafer/Reservation/ReservationController.java
+++ src/main/java/ir/najaftech/najafer/Reservation/ReservationController.java
@@ -1,8 +1,17 @@
 package ir.najaftech.najafer.Reservation;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/reservation")
 public class ReservationController {
+    private ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    
     
 }
