68ea8c7f5c50e91489589f7c31d4deaee384b56d
Ali najafian <alinajafian580@gmail.com>
2024-06-04 15:20:26 +0330

    ReservationService dependency injection complete

--- src/main/java/ir/najaftech/najafer/Reservation/ReservationService.java
+++ src/main/java/ir/najaftech/najafer/Reservation/ReservationService.java
@@ -1,10 +1,24 @@
 package ir.najaftech.najafer.Reservation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ir.najaftech.najafer.Doctor.DoctorRepository;
+import ir.najaftech.najafer.User.UserRepository;
 
 @Service
 public class ReservationService {
 
-    
+    private DoctorRepository doctorRepository;
+    private UserRepository userRepository;
+    private ReservationRepository reservationRepository;
+
+    @Autowired
+    public ReservationService(DoctorRepository doctorRepository, UserRepository userRepository,
+            ReservationRepository reservationRepository) {
+        this.doctorRepository = doctorRepository;
+        this.userRepository = userRepository;
+        this.reservationRepository = reservationRepository;
+    }
     
 }
