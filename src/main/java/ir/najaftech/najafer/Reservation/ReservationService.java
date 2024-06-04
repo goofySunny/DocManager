354c3124152bc57b87a0b75b3fbb0925c17b2d72
Ali najafian <alinajafian580@gmail.com>
2024-06-04 15:28:19 +0330

    implemented basic service methods

--- src/main/java/ir/najaftech/najafer/Reservation/ReservationService.java
+++ src/main/java/ir/najaftech/najafer/Reservation/ReservationService.java
@@ -1,9 +1,12 @@
 package ir.najaftech.najafer.Reservation;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
+import ir.najaftech.najafer.Doctor.Doctor;
 import ir.najaftech.najafer.Doctor.DoctorRepository;
+import ir.najaftech.najafer.User.User;
 import ir.najaftech.najafer.User.UserRepository;
 
 @Service
@@ -21,4 +24,25 @@ public class ReservationService {
         this.reservationRepository = reservationRepository;
     }
     
+    public Reservation addReservation(String doctorId, String userId, Reservation reservation) {
+        Doctor doctor = doctorRepository.findById(doctorId).orElseThrow(() -> new UsernameNotFoundException("Doctor not found"));
+        User user = userRepository.findById(userId).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        reservation.setDoctor(doctor);
+        reservation.setUser(user);
+        reservationRepository.save(reservation);
+        return reservation; 
+    }
+
+    public Reservation getReservation(String id) {
+        return reservationRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("No such reservation"));
+    }
+
+    public void deleteReservation(String id) {
+        reservationRepository.deleteById(id);
+    }
+
+    public List<Reservation> retrieveAllReservations() {
+        return reservationRepository.findAll();
+    }
+
 }
