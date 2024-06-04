77ea4d18304cc5a9b8524352dbc07d3c24fa9d0e
Ali najafian <alinajafian580@gmail.com>
2024-06-04 16:36:08 +0330

    Reservation controller completed with basic methods

--- src/main/java/ir/najaftech/najafer/Reservation/ReservationController.java
+++ src/main/java/ir/najaftech/najafer/Reservation/ReservationController.java
@@ -1,7 +1,13 @@
 package ir.najaftech.najafer.Reservation;
 
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/reservation")
@@ -12,6 +18,20 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
+    @PostMapping("/{doctorId}/{userId}")
+    public ResponseEntity<Reservation> createReservation(@PathVariable String doctorId, @PathVariable String userId, @RequestBody Reservation reservation) {
+        Reservation reserved = reservationService.addReservation(doctorId, userId, reservation);
+        return ResponseEntity.ok(reserved);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> retrieveAllReservations() {
+        List<Reservation> reservations = reservationService.retrieveAllReservations();
+        return ResponseEntity.ok(reservations);
+    }
     
-    
+    @GetMapping("/{id}")
+    public ResponseEntity<Reservation> retrieveReservation(@PathVariable String id) {
+        return ResponseEntity.ok(reservationService.getReservation(id));
+    }
 }
