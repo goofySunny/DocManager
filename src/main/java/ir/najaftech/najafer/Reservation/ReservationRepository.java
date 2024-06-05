e3b5d55e6dea7106b40d60fb8c2e021e0abe495b
Ali najafian <alinajafian580@gmail.com>
2024-06-05 15:29:04 +0330

    Added extra find methods

--- src/main/java/ir/najaftech/najafer/Reservation/ReservationRepository.java
+++ src/main/java/ir/najaftech/najafer/Reservation/ReservationRepository.java
@@ -1,10 +1,18 @@
 package ir.najaftech.najafer.Reservation;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import ir.najaftech.najafer.Doctor.Doctor;
+import ir.najaftech.najafer.User.User;
 
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, String> {
     
+    List<Reservation> findAllByUser(User user);
+    List<Reservation> findAllByDoctor(Doctor doctor);
+
 }
