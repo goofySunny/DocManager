6487d6a24a0e86cffa9cb263ea05c23a3dcecab0
Ali najafian <alinajafian580@gmail.com>
2024-06-07 17:03:25 +0330

    added CustomizedResponseEntityExceptionHandler.java for handling exceptions (duh)

---
+++ src/main/java/ir/najaftech/najafer/Exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,23 @@
+package ir.najaftech.najafer.Exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest req) {
+        ErrorDetails err = new ErrorDetails(ex.getMessage(), LocalDateTime.now(), req.getDescription(false));
+        return new ResponseEntity<Object>(err, HttpStatus.UNAUTHORIZED);
+    }
+
+    
+}
