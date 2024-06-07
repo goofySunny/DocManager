4c5c128088777abccab57fc7d3e44c966ab2aab3
Ali najafian <alinajafian580@gmail.com>
2024-06-07 17:06:16 +0330

    added PastReservationException.java

---
+++ src/main/java/ir/najaftech/najafer/Exception/PastReservationException.java
@@ -0,0 +1,7 @@
+package ir.najaftech.najafer.Exception;
+
+public class PastReservationException extends RuntimeException {
+    public PastReservationException(String message) {
+        super(message);
+    }
+}
