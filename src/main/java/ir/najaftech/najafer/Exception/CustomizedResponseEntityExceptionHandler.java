910d9d2019837850915f82c956a1ce1b9015bff9
Ali najafian <alinajafian580@gmail.com>
2024-06-08 11:12:24 +0330

    Added ExceptionHandler methods for the added Exceptions

--- src/main/java/ir/najaftech/najafer/Exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/ir/najaftech/najafer/Exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,7 +2,9 @@ package ir.najaftech.najafer.Exception;
 
 import java.time.LocalDateTime;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,5 +21,20 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         return new ResponseEntity<Object>(err, HttpStatus.UNAUTHORIZED);
     }
 
-    
+    @ExceptionHandler(InvalidDetailsException.class)
+    public final ResponseEntity<Object> handleInvalidDetailsException(Exception ex, WebRequest req) {
+        ErrorDetails err = new ErrorDetails(ex.getMessage(), LocalDateTime.now(), req.getDescription(false));
+        return new ResponseEntity<Object>(err, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(PastReservationException.class)
+    public final ResponseEntity<Object> handlePastReservationException(Exception ex, WebRequest req) {
+        ErrorDetails err = new ErrorDetails(ex.getMessage(), LocalDateTime.now(), req.getDescription(false));
+        return new ResponseEntity<Object>(err, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public final ResponseEntity<Object> handleUserAlreadyExistsException(Exception ex, WebRequest req) {
+        ErrorDetails err = new ErrorDetails(ex.getMessage(), LocalDateTime.now(), req.getDescription(false));
+        return new ResponseEntity<Object>(err, HttpStatus.BAD_REQUEST);
+    }
 }
