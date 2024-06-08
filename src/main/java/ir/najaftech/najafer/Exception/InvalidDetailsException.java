a3d41de7f9518a9fc59f7e9b317a8631a0a46d5c
Ali najafian <alinajafian580@gmail.com>
2024-06-08 11:11:33 +0330

    InvalidDetailsException.java

---
+++ src/main/java/ir/najaftech/najafer/Exception/InvalidDetailsException.java
@@ -0,0 +1,8 @@
+package ir.najaftech.najafer.Exception;
+
+public class InvalidDetailsException extends RuntimeException {
+    public InvalidDetailsException(String message) {
+        super(message);
+    }
+    
+}
