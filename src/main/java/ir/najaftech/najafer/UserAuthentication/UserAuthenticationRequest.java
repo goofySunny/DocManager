3639203e3adf5bd905fea9158ed4d51946292a8e
Ali najafian <alinajafian580@gmail.com>
2024-06-27 18:10:00 +0330

    added jakarta validation constraint annotations to this class for validation of the pojos before actual save in the repo (we dont want bad credentials)

--- src/main/java/ir/najaftech/najafer/UserAuthentication/UserAuthenticationRequest.java
+++ src/main/java/ir/najaftech/najafer/UserAuthentication/UserAuthenticationRequest.java
@@ -2,6 +2,12 @@ package ir.najaftech.najafer.UserAuthentication;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class UserAuthenticationRequest {
     private String userEmail;
     private String userPassword;
@@ -28,11 +34,24 @@ public class UserAuthenticationRequest {
     }
 
     public static class RegisterationRequest {
+        @Email
+        @NotNull(message = "This field is required")
+        @NotEmpty(message = "This field is required")
         private String userEmail;
+        @NotEmpty(message = "This field is required")
+        @NotNull(message = "This field is required")
         private String username;
+        @Size(min = 8, max = 24, message = "Password must be between 8 and 24 characters")
+        @NotEmpty(message = "This field is required")
+        @NotNull(message = "This field is required")
         private String userPassword;
+        @Past(message = "Bro?")
         private LocalDate dob;
+        @NotEmpty(message = "This field is required")
+        @NotNull(message = "This field is required")
         private String name;
+        @NotEmpty(message = "This field is required")
+        @NotNull(message = "This field is required")
         private String lastName;
 
         
