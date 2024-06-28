0c65e758d51f3fd05df758146e9e33da83e9dd30
Ali najafian <alinajafian580@gmail.com>
2024-06-28 14:23:55 +0330

    Addition: small ExceptionHandler method copied to both of these classes for validation exception handling (I know its redundant code but I failed to make it work in the @ControllerAdvice class, will work on it later)

--- src/main/java/ir/najaftech/najafer/DoctorAuthentication/DoctorAuthenticationController.java
+++ src/main/java/ir/najaftech/najafer/DoctorAuthentication/DoctorAuthenticationController.java
@@ -1,10 +1,18 @@
 package ir.najaftech.najafer.DoctorAuthentication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -30,4 +38,15 @@ public class DoctorAuthenticationController {
         return ResponseEntity.ok(doctorAuthenticationService.register(request));
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public final Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return errors;
+    }
 }
--- src/main/java/ir/najaftech/najafer/UserAuthentication/UserAuthenticationController.java
+++ src/main/java/ir/najaftech/najafer/UserAuthentication/UserAuthenticationController.java
@@ -1,9 +1,17 @@
 package ir.najaftech.najafer.UserAuthentication;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -30,5 +38,17 @@ public class UserAuthenticationController {
     @PostMapping("register")
     public ResponseEntity<UserAuthenticationResponse> register(@Valid @RequestBody UserAuthenticationRequest.RegisterationRequest credentials) {
         return ResponseEntity.ok().body(userAuthenticationService.register(credentials));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public final Map<String, String> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return errors;
     }
 }
